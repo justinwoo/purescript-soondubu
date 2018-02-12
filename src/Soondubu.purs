26fac554723e98710dfbeed1585a7307f4e2ecfd
justinwoo <moomoowoo@gmail.com>
2018-02-12 20:20:46 +0200

    use opaque data types so i don't have to deal with unresolved types

--- src/Soondubu.purs
+++ src/Soondubu.purs
@@ -2,36 +2,48 @@ module Soondubu where
 
 import Control.Monad.Eff (Eff)
 import Graphics.Canvas (Context2D)
+import Unsafe.Coerce (unsafeCoerce)
 
 foreign import data ChartInstance :: Type
+foreign import data BarData :: Type
+foreign import data BarDataset :: Type
 
 foreign import makeBarChart_ :: forall barChartSpec e
    . barChartSpec -> Context2D -> Eff e ChartInstance
 
 makeBarChart
-  :: forall e options
-       spec spec'
-       data_ data_'
-       dataset dataset'
-   . Union spec spec' (BarSpec data_ options)
-  => Union data_ data_' (BarSpecData dataset)
-  => Union dataset dataset' BarSpecDataset
+  :: forall e options spec spec'
+   . Union spec spec' (BarSpec options)
   => Record spec
   -> Context2D
   -> Eff e ChartInstance
 makeBarChart = makeBarChart_
 
+makeBarData
+  :: forall data_ data_'
+   . Union data_ data_' BarSpecData
+  => Record data_
+  -> BarData
+makeBarData = unsafeCoerce
+
+makeBarDataset
+  :: forall dataset dataset'
+   . Union dataset dataset' BarSpecDataset
+  => Record dataset
+  -> BarDataset
+makeBarDataset = unsafeCoerce
+
 type ChartSpec data_ options =
-  ( "data" :: Record data_
-  , options :: Record options
+  ( "data" :: data_
+  , options :: options
   )
 
-type BarSpec data_ options =
-  ChartSpec data_ options
+type BarSpec options =
+  ChartSpec BarData (Record options)
 
-type BarSpecData dataset =
+type BarSpecData =
   ( labels :: Array String
-  , datasets :: Array (Record dataset)
+  , datasets :: Array BarDataset
   )
 
 type BarSpecDataset =
